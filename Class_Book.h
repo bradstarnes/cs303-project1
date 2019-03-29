c37c2fac5f81b95eff788c7cc490d0add482e237
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-03-29 15:03:06 -0500

    Update and rename book.h to Class_Book.h

---
+++ Class_Book.h
@@ -0,0 +1,15 @@
+//
+// Created by Brad Starnes on 2019-03-14.
+//
+
+#ifndef UNTITLED_BOOK_CLASS_H
+#define UNTITLED_BOOK_CLASS_H
+
+
+class Class_Book {
+
+};
+
+
+#endif //UNTITLED_BOOK_CLASS_H
+
