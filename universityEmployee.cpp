95f587c424838f1b8555a427009bf30e02cea98d
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-03-28 19:00:59 -0500

    Created Class Internals
    * Created Class Attributes
    * Created Initial Class Methods for Class Operations

--- universityEmployee.cpp
+++ universityEmployee.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "universityEmployee.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+class employee{
+
+    string fName;
+    string lName;
+    int empID = rand()%(5999999-1000000 + 1) + 1000000; // Create Random 7 Digit Employee ID
+    int bookCnt = 0;
+    int queuePosition;
+    time_t retentionTimeTotal;
+    time_t retentionTimeAvg;
+
+    time_t getTime(){
+        time_t now = time(0);
+    }
+    void timeHeld(time_t rcvd){
+        this->retentionTimeTotal += getTime() - rcvd;
+        if (bookCnt > 0)
+            this->retentionTimeAvg = this->retentionTimeTotal / this->bookCnt;
+
+    }
+    
+};
