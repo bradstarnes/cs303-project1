317135ad50570f9f18ac3ecfbf6c6c146968a773
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-03-29 16:00:16 -0500

    Cleaned up and Refactored the Methods and Attributes

--- Class_Employee.cpp
+++ Class_Employee.cpp
@@ -2,7 +2,7 @@
 // Created by Adam Smith on 2019-03-29.
 //
 
-#include "Employee_Class.h"
+#include "Class_Employee.h"
 #include <iostream>
 #include <ctime>
 #include <iomanip>
@@ -15,50 +15,43 @@ public:
     string lName;
     int empID;
     int bookCnt;
-    int queuePosition;
     int priority;
     time_t receivedCurrentBook;
-    time_t retentionTimeTotal;
-    time_t retentionTimeCurrent;
-    time_t retentionTimeAvg;
+    time_t passedOnBook;
+    time_t timeWithBook;
+    time_t timeWithOutBook;
 
-    employee(){
+    employee(string fName, string lName){
 
-        int empID = rand()%(5999999-1000000 + 1) + 1000000; // Create Random 7 Digit Employee ID
-        int bookCnt = 0;
-        int priority = getPriority();
+        this->fName = fName;
+        this->lName = lName;
 
+        empID = rand()%(5999999-1000000 + 1) + 1000000; // Create Random 7 Digit Employee ID
+        bookCnt = 0;
+        timeWithBook = 0;
+        timeWithOutBook = 0;
+        passedOnBook = 0;
+        receivedCurrentBook = 0;
+        priority = getPriority();
     }
-
 
     time_t getTime(){
         time_t now = time(0);
     }
-    void timeHeld_transfer(time_t rcvd){
-        //***** DO NOT CALL UNLESS TRANSFERING the Book *****
-        //***** Retention Time Total will break *****
-        //Updates Average Time Held
-        //Updates Total Time Held
-        //Updates Total Time Holding current book
-
-        time_t timeDiff = getTime() - rcvd;//Time passed since receiving the book
-
-        this->retentionTimeTotal += timeDiff;
-        this->retentionTimeCurrent = timeDiff;
-        if (bookCnt > 0)
-            this->retentionTimeAvg = this->retentionTimeTotal / this->bookCnt;
-    }
-    int getPriority(){
+    void passOn(){
         time_t now = getTime();
-        int priority = 0;
-        timeHeld(now);
-        priority = now - timeHeld(now);
-
+        this->passedOnBook = now;
+        this->timeWithBook += now - receivedCurrentBook;
+    }
+    void takeOn(){
+        time_t now = getTime();
+        this->receivedCurrentBook = now;
+        this->timeWithOutBook += now - passedOnBook;
+    }
+    double getPriority(){
+        double priority = difftime(timeWithOutBook, timeWithBook);
         return priority;
     }
 
+
 };
-int main(){
-    employee *testEmp = new employee();
-    cout << testEmp->empID;
-}
