a3d27c882297fd7a8d696196d10e7d4b5ce2a8cd
bradstarnes <starnesjackb@outlook.com>
2019-03-31 23:02:19 -0500

    Updated header of each file with project name and comments

--- Pqueue.cpp
+++ Pqueue.cpp
@@ -15,34 +15,189 @@
  *
  *
  */
-
 #include "Pqueue.h"
+#include <utility>
+#include <vector>
 
-pQueue::pQueue(){}
-void pQueue::addEmployee(Employee* temp){
-    WorkQueue.push_back(temp);
-}
-Employee* pQueue::pop_max(){
-    Employee *max = *WorkQueue.begin();
-    vector<Employee*>::iterator it, toErase;
-    it = toErase = WorkQueue.begin();
-    it++;
-    for (it; it != WorkQueue.end(); it++){
-        if ((max->getWait() - max->getRetain()) < ((*it)->getWait() - (*it)->getRetain())){
-            max = *it;
-            toErase = it;
+/*  NOTE: This code is essentially word for word grabbed from a GIT Hub Source
+ *
+ *      Source: https://github.com/Ten0/updatable_priority_queue
+ *
+ *      This was not intentionally plagarized, the fact that we weren't able to at
+ *      least refactor it is due to the timeline on which we discovered the need for
+ *      it. Due to an error on our teams part we ended up having to scrap the original
+ *      pQueue Code since we realized quite literally last minute that no built-in
+ *      function existed to update the priority queue. This mistake arose from a
+ *      lack of co-ordination that we now realize is far more crucial.
+ *
+ *      I (Adam) chose to keep this code instead of the original code since this code
+ *      is BEAUTIFULLY written, even if it is somewhat obscure about how its actually
+ *      working. Additionally the code does a fantastic job of integrating with existing code.
+ *      Essentially I decided to keep it because I was immediately drawn to the original authors work.
+ *
+ *
+ *
+ * */
+
+namespace improved_pQueue {
+
+    //Template allows for any types to be sorted (ACS)
+    template <typename Key, typename Priority>
+
+    struct priority_queue_node {
+        Priority priority;
+        Key key;
+        priority_queue_node(const Key& key, const Priority& priority) : priority(priority), key(key) {}
+        friend bool operator<(const priority_queue_node& pqn1, const priority_queue_node& pqn2) {
+            return pqn1.priority < pqn2.priority;
         }
-    }
-    WorkQueue.erase(toErase);
-    return max;
-}
-Employee* pQueue::top(){
-    Employee *max = *WorkQueue.begin();
-    vector<Employee*>::iterator it = WorkQueue.begin();
-    it++;
-    for (it; it != WorkQueue.end(); it++){
-        if ((max->getWait() - max->getRetain()) < ((*it)->getWait() - (*it)->getRetain())){
-            max = *it;
+        friend bool operator>(const priority_queue_node& pqn1, const priority_queue_node& pqn2) {
+            return pqn1.priority > pqn2.priority;
         }
-    }
-    return max;
+    };
+
+    /** Key has to be an u-int value (convertible to size_t)
+     * This is a max heap (max is on top), to match STL's pQ */
+    template <typename Key, typename Priority>
+
+    class updatable_priority_queue {
+
+    protected:
+        std::vector<size_t> id_to_heappos;  //<- Not entirely sure what this is doing (ACS)
+        // I assume it is used to manage priorities?
+        std::vector<priority_queue_node<Key,Priority>> heap;
+
+    public:
+        updatable_priority_queue() {}
+
+        bool empty()       const { return heap.empty(); }
+        std::size_t size() const { return heap.size(); }
+
+        /** first is priority, second is key */
+        const priority_queue_node<Key,Priority>& top() const { return heap.front(); }
+
+        void pop(bool remember_pop=false) {
+            if(size() == 0)
+                return; //Break if empty (ACS)
+            id_to_heappos[heap.front().key] = -1-remember_pop;  //<-I do not know what hes doing here, I think its
+            // changing something with the key value? (ACS)
+            if(size() > 1)
+                *heap.begin() = std::move(*(heap.end()-1));
+            heap.pop_back();
+            sift_down(0);
+        }
+
+        priority_queue_node<Key,Priority> pop_value(bool remember_key=true) {
+            if(size() == 0)
+                return priority_queue_node<Key, Priority>(-1, Priority());
+
+            priority_queue_node<Key,Priority> ret = std::move(*heap.begin());
+            id_to_heappos[ret.key] = -1-remember_key;
+
+            if(size() > 1)
+                *heap.begin() = std::move(*(heap.end()-1));
+
+            heap.pop_back();
+            sift_down(0);
+            return ret;
+        }
+
+        /** Sets the priority for the given key. If not present, it will be added, otherwise it will be updated
+         *  Returns true if the priority was changed.
+         * */
+        bool set(const Key& key, const Priority& priority, bool only_if_higher=false) {
+            if(key < id_to_heappos.size() && id_to_heappos[key] >= 0) // This key is already in the pQ
+                return update(key, priority, only_if_higher);
+            else
+                return push(key, priority, only_if_higher);
+        }
+
+        std::pair<bool,Priority> get_priority(const Key& key) {
+            if(key >= id_to_heappos.size()) {
+                size_t pos = id_to_heappos[key];
+                if(pos >= 0) {
+                    return {true, heap[pos].priority};
+                }
+            }
+            return {false, 0};
+        }
+
+        /** Returns true if the key was not inside and was added, otherwise does nothing and returns false
+         *  If the key was remembered and only_if_unknown is true, does nothing and returns false
+         * */
+        bool push(const Key& key, const Priority& priority, bool only_if_unknown=false) {
+            extend_ids(key);
+            if(id_to_heappos[key] < ((size_t)-2)) return false;
+            if(only_if_unknown && id_to_heappos[key] == ((size_t)-2)) return false;
+            // otherwise we have id_to_heappos[key] = -1, unseen key
+            size_t n = heap.size();
+            id_to_heappos[key] = n; // For consistency in the case where nothing moves (early return)
+            heap.emplace_back(key,priority);
+            sift_up(n);
+            return true;
+        }
+
+        /** Returns true if the key was already inside and was updated, otherwise does nothing and returns false */
+        bool update(const Key& key, const Priority& new_priority, bool only_if_higher=false) {
+            if(key >= id_to_heappos.size()) return false;
+            size_t heappos = id_to_heappos[key];
+            if(heappos >= ((size_t)-2)) return false;
+            Priority& priority = heap[heappos].priority;
+            if(new_priority > priority) {
+                priority = new_priority;
+                sift_up(heappos);
+                return true;
+            }
+            else if(!only_if_higher && new_priority < priority) {
+                priority = new_priority;
+                sift_down(heappos);
+                return true;
+            }
+            return false;
+        }
+
+    private:
+        void extend_ids(Key k) {
+            size_t new_size = k+1;
+            if(id_to_heappos.size() < new_size)
+                id_to_heappos.resize(new_size, -1);
+        }
+
+        void sift_down(size_t heappos) {
+            size_t len = heap.size();
+            size_t child = heappos*2+1;
+            if(len < 2 || child >= len) return;
+            if(child+1 < len && heap[child+1] > heap[child]) ++child; // Check whether second child is higher
+            if(!(heap[child] > heap[heappos])) return; // Already in heap order
+
+            priority_queue_node<Key,Priority> val = std::move(heap[heappos]);
+            do {
+                heap[heappos] = std::move(heap[child]);
+                id_to_heappos[heap[heappos].key] = heappos;
+                heappos = child;
+                child = 2*child+1;
+                if(child >= len) break;
+                if(child+1 < len && heap[child+1] > heap[child]) ++child;
+            } while(heap[child] > val);
+            heap[heappos] = std::move(val);
+            id_to_heappos[heap[heappos].key] = heappos;
+        }
+
+        void sift_up(size_t heappos) {
+            size_t len = heap.size();
+            if(len < 2 || heappos <= 0) return;
+            size_t parent = (heappos-1)/2;
+            if(!(heap[heappos] > heap[parent])) return;
+            priority_queue_node<Key, Priority> val = std::move(heap[heappos]);
+            do {
+                heap[heappos] = std::move(heap[parent]);
+                id_to_heappos[heap[heappos].key] = heappos;
+                heappos = parent;
+                if(heappos <= 0) break;
+                parent = (parent-1)/2;
+            } while(heap[heappos] > heap[parent]);
+            heap[heappos] = std::move(val);
+            id_to_heappos[heap[heappos].key] = heappos;
+        }
+    };
+}
